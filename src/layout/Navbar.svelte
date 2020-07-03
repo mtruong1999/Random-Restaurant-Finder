dbbcde5c4fbed8e1d07347bb08a7370357e7df82
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-07-03 00:05:23 -0600

    Add collapsible menu button

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -1,11 +1,17 @@
 <script>
     import {Link} from 'svelte-routing';
+    // doesnt work, look into menu opening on click
+    document.addEventListener('DOMContentLoaded', function(){
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, options);
+    });
 </script>
 
 <nav>
     <div class="nav-wrapper deep-purple lighten-2">
         <div class="container">
             <Link to="/"><span class="brand-logo">Random Restaurant</span></Link>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
@@ -13,3 +19,8 @@
         </div>
     </div>
 </nav>
+
+<ul class="sidenav" id="mobile-demo">
+    <li><Link to="/">Home</Link></li>
+    <li><Link to="/about">About</Link></li>
+</ul>
