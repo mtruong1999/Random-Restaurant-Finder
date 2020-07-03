7583f0cc7def3e1d49cae5da753048c774f086c7
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-07-02 23:25:01 -0600

    First commit

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {Link} from 'svelte-routing';
+</script>
+
+<nav>
+    <div class="nav-wrapper deep-purple lighten-2">
+        <div class="container">
+            <Link to="/"><span class="brand-logo">Random Restaurant</span></Link>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </div>
+    </div>
+</nav>
