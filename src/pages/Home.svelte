d0794328e783ecb958914f55edccbdafa7a0ac12
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-07-03 00:05:46 -0600

    Add radius buttons

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,4 +1,5 @@
 <script>
+    let radius = 5;
 
 </script>
 
@@ -6,4 +7,29 @@
 
 </style>
  
-<h1>Home</h1>
+<h1>Random Eater</h1>
+
+<div class="row">
+    <div class="col s12">
+        <form action="#">
+            <p>
+                <label>
+                    <input name="fivemiles" type="radio" bind:group={radius} value=5>
+                    <span>5 miles</span>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <input name="tenmiles" type="radio" bind:group={radius} value=10> 
+                    <span>10 miles</span>
+                </label>
+            </p>
+            <p>
+                <label>
+                    <input name="twentymiles" type="radio" bind:group={radius} value=20>
+                    <span>20 miles</span>
+                </label>
+            </p>
+        </form>
+    </div>
+</div>
