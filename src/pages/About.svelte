485b164a50ce153b5bbfca931e8bbe75d434c794
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-07-04 12:53:03 -0600

    Update about page

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -1,1 +1,11 @@
 <h1>About</h1>
+<p class="flow-text">One common problem that exists when eating out is indecisiveness
+when choosing a restaurant. This app was created to provide a fun solution to this problem
+by providing you a random option. Go with the first option given to you to be spontaneuous or,
+if you're REALLY indecisive, keep generating random restaurants until you see what you've
+really wanted to eat all along but were too afraid to admit it.</p>
+<p class="flow-text">More features will be coming soon! In the meantime, enjoy this montage
+I found online of the cabbage man from Avatar the Last Airbender :).</p>
+<div class="video-container">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/C2YCs52XLjs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
