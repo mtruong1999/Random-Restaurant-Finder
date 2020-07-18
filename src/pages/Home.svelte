53e36cf14e46a67c3db18c00a0ca45a8e3f893af
Michael <36463000+mtruong1999@users.noreply.github.com>
2020-07-18 12:50:15 -0600

    Add loading animation to fetch
    Added a materializecss indeterminate loading to replace all input fields when fetching for a random restaurant

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -6,6 +6,7 @@
     let zip = null;
     let readyToSubmit = false; // set to true when have a location AND radius? or use default radius?
     let locationFetched = false;
+    let loading = false;
 
     let long = null;
     let lat = null;
@@ -65,32 +66,9 @@
     }
     function onGetRestaurant(event) {
         console.log(zip);
-        /*
-        if(zip !== null || locationFetched)
-        {
-            // have required info, get nearby restaurants
-            if(zip !== null) // might not need this check because btn should be disabled
-            {
-                // zip field not empty, get long/lat for zip
-                fetch(`${apiBaseUrl}postalcode=${zip}&username=${un}&country=us&type=json`)
-                    .then(status)
-                    .then(res => {
-                        return res.json();
-                    })
-                    .then((data) => {
-                        lat = data.postalCodes[0].lat;
-                        long = data.postalCodes[0].lng;
-                    }).catch((error) => {
-                        // TODO: do something better when error
-                        console.log('Request failed', error);
-                    });
-            }
-        }
-        */
-        // Dont need zip anymore since can be passed to yelp api
-        // so we can instead check whether we are wanting to use
-        // zip or lat/long in request
+        // TODO: Move this to a separate component
         let url;
+        loading = true;
         if(locationFetched)
         {
             url = `${yelpApiBaseUrl}?latitude=${lat}&longitude=${long}`;
@@ -118,6 +96,8 @@
             });
         // TODO: Add conversion from miles to meters for GET request
 
+        // Done
+        loading = false;
     }
 
 </script>
@@ -127,36 +107,42 @@
 </style>
  
 <h1>Random Restaurant</h1>
-
-<div class="row">
-    <div class="col s6">
-        <div class="input-field">
-            <label for="zip-code">Zip code</label>
-            <input type="text" bind:value={zip} on:input={handleZip}/>
+{#if !loading}
+    <div class="row">
+        <div class="col s6">
+            <div class="input-field">
+                <label for="zip-code">Zip code</label>
+                <input type="text" bind:value={zip} on:input={handleZip}/>
+            </div>
+            <button class="waves-effect waves-light btn" on:click={getLocation}>
+                <i class="material-icons left">location_on</i>
+                Use my current location
+            </button>
+            <p>{@html location_string}</p>
         </div>
-        <button class="waves-effect waves-light btn" on:click={getLocation}>
-            <i class="material-icons left">location_on</i>
-            Use my current location
-        </button>
-        <p>{@html location_string}</p>
+        <div class="col s6">
+            <RadiusInput bind:radius={radius}/>
+            <button class="btn" on:click={handlebtn}>CLICK</button>
+        </div>
     </div>
-    <div class="col s6">
-        <RadiusInput bind:radius={radius}/>
-        <button class="btn" on:click={handlebtn}>CLICK</button>
+    <div class="row">
+        <div class="col s6">
+            {#if readyToSubmit}
+                <button class="waves-effect waves-light pulse btn" on:click={onGetRestaurant}>
+                    <i class="material-icons left">restaurant</i>
+                    Get restaurant
+                </button>
+            {:else}
+                <button class="btn disabled">
+                    <i class="material-icons left">restaurant</i>
+                    Get restaurant
+                </button>
+            {/if}
+        </div>
     </div>
-</div>
-<div class="row">
-    <div class="col s6">
-        {#if readyToSubmit}
-            <button class="waves-effect waves-light pulse btn" on:click={onGetRestaurant}>
-                <i class="material-icons left">restaurant</i>
-                Get restaurant
-            </button>
-        {:else}
-            <button class="btn disabled">
-                <i class="material-icons left">restaurant</i>
-                Get restaurant
-            </button>
-        {/if}
+{:else}
+    <div class="progress">
+        <div class="indeterminate"></div>
     </div>
-</div>
+    
+{/if}
